0ebeceae336ce86ee38f4955514a13bd24085ab7
timothyshen <timothyshen1@hotmail.com>
2024-02-29 18:37:39 -0700

    adding all contract deployment

--- frontend/contract/contractAddress.ts
+++ frontend/contract/contractAddress.ts
@@ -1,6 +1,19 @@
-export const MODEL_NFT_ADDRESS = "0x3c259f9805847d85bc720d5615128832c2987ef3";
-export const LICENCE_NFT_ADDRESS = "0x896a9e2a9769e43e085bfa88ddc477b359e89a0a";
+export const MODEL_NFT_ADDRESS = "0x52f505378872ac171e778f92b1757f24cb0de0f3";
+export const LICENCE_NFT_ADDRESS = "0x31f2292f91bfa66dc8ca3c5047b2fe9b6b7c5ab6";
 export const IPAREGISTRAR_ADDRESS =
-  "0x0919ba4768d448a1944a2b9c0cb08dc05eef9469";
+  "0x75a80c281796f4ea94f937881d9d83f03f90babd";
 export const IPAREMIXREGISTRAR_ADDRESS =
-  "0x896a9e2a9769e43e085bfa88ddc477b359e89a0a";
+  "0x877cdc6441a7332237994ef3d43c3eddd35dfc12";
+export const POLICYREGISTRAR_ADDRESS =
+  "0xf7934eab3d033fc01604b8e4eb4c1acc09b50584";
+export const LICENSINGREGISTRAR_ADDRESS =
+  "0xb90b917245b21080a4692bf46a9e9a98cc412302";
+
+/**
+ * deployed ModelNFT to 0x52f505378872ac171e778f92b1757f24cb0de0f3
+ * deployed LicenseNFT to 0x31f2292f91bfa66dc8ca3c5047b2fe9b6b7c5ab6
+ * deployed ModelNFTRemix to 0x877cdc6441a7332237994ef3d43c3eddd35dfc12
+ * deployed ModelNFTRegistrar to 0x75a80c281796f4ea94f937881d9d83f03f90babd
+ * deployed PolicyRegistrar to 0xf7934eab3d033fc01604b8e4eb4c1acc09b50584
+ * deployed LicensingRegistrar to 0xb90b917245b21080a4692bf46a9e9a98cc412302
+ **/
