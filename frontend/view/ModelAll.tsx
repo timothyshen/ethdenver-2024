5765c6f8512006d39dee4ff70219a4596b8cb23b
timothyshen <timothyshen1@hotmail.com>
2024-03-01 18:08:13 -0700

    add fix

--- frontend/view/ModelAll.tsx
+++ frontend/view/ModelAll.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import { ViewAllModel } from '@/view/ViewAllModel'
 import { ViewAllModelCreator } from '@/view/ViewAllModelCreater'
--- frontend/view/PolicyModel.tsx
+++ frontend/view/PolicyModel.tsx
@@ -5,7 +5,7 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-} from "@/components/ui/dialog"
+} from ""
 import { CreatePolicy } from "@/components/CreatePolicy"
 import { Button } from "@/components/ui/button"
 
